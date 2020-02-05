3a969ab7d3d0339bc8db2182ab12665dbe0dc1d7
Lee Painton <leepainton@google.com>
2020-02-05 15:49:53 -0500

    Split some main logic into a server object

---
+++ server/server.go
@@ -0,0 +1,88 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+type server struct {
+	token string
+	sync.Mutex
+}
+
+const apiToken = "xoxb-51553666834-887269487187-8gqXxYVqA0hAW67Zscmbc6d7"
+
+type command int
+type channel string
+type player string
+
+const (
+	play command = iota
+	flip command = iota
+	bid  command = iota
+	liar command = iota
+)
+
+//map[response_url:[https://hooks.slack.com/commands/T1HG9KLQJ/886014563746/HarTowFqJ7KSgU9602DtxwZf] team_id:[T1HG9KLQJ] team_domain:[aviatodev] channel_id:[C1HEJF0DR] text:[] token:[uywRTb17a4I313NX3DLGx9R3] user_id:[U78H0HHPG] command:[/hello] user_name:[lee.painton] channel_name:[random] trigger_id:[900657739207.51553666834.5eff0db620d4fda7f5752e0352182959]]
+type message struct {
+	command     command
+	channelID   channel
+	responseURL *url.URL
+	text        string
+	userID      player
+	userName    string
+}
+
+func (s *server) apiHandler(w http.ResponseWriter, r *http.Request) {
+	s.Lock()
+
+	/*m, err := parseMsg(r)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), 400)
+		return
+	}*/
+
+	switch r.URL.Path {
+	case "/play":
+		//m.command = play
+		fmt.Fprintln(w, "Joined game")
+	case "/flip":
+		//m.command = flip
+		fmt.Fprintln(w, "Flipped game")
+	case "/bid":
+		//m.command = bid
+		fmt.Fprintln(w, "Bidding")
+	case "/liar":
+		//m.command = liar
+		fmt.Fprintln(w, "Liar!")
+	default:
+		fmt.Fprintln(w, "Error!")
+	}
+
+	s.Unlock()
+}
+
+func parseMsg(r *http.Request) (*message, error) {
+	r.ParseForm()
+	for _, f := range []string{
+		"channel_id",
+		"text",
+		"user_id",
+		"user_name",
+	} {
+		if v := r.Form[f]; len(v) == 0 {
+			err := fmt.Errorf("missing field %q in api call", f)
+			return nil, err
+		}
+	}
+
+	return &message{
+		channelID: channel(r.Form["channel_id"][0]),
+		text:      r.Form["text"][0],
+		userID:    player(r.Form["user_id"][0]),
+		userName:  r.Form["user_name"][0],
+	}, nil
+}
